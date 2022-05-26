29d9af7bd54e302b29e25f3e4498ecc63728d227
Shubham Kumar <kshubhambgs123@gmail.com>
2022-05-26 18:32:30 +0530

    Time: 0 ms (100.00%), Space: 6 MB (30.60%) - LeetHub

---
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        
+        int ans = 0;
+        for(int i=5;i<=n;i=i*5){
+            ans=ans+n/i;
+        }
+        return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         long long int fact = 1;
+//         for(long long int i=2;i<=n;i++){
+//             fact = fact*i;
+//         }
+//         int res = 0;
+        
+//         while(fact%10==0){
+//             res++;
+//             fact=fact/10;
+//         }
+//         return res;
+    }
+};
