855a5c4e39bea2f36647f0bd1f7859b857d409dc
Shubham Kumar <kshubhambgs123@gmail.com>
2022-06-01 12:04:16 +0530

    Time: 27 ms (85.37%), Space: 22.2 MB (91.24%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int l = 0;
-        int h = nums.size()-1;
-        while(l<h){
-            int mid = l+(h-l)/2;      // divide the array
+        int l = 0, h = nums.size() - 1;
+        while (l < h) {
+            int mid = l + (h - l) / 2;
             
-            if(nums[mid]==nums[mid+1]) mid = mid-1;      //two same elements should be in same // half
             
-            if((mid-l+1)%2!=0) h = mid;            // checking the length of left half. If its is odd then update ur right pointer to mid
+            if (mid % 2 && nums[mid - 1] == nums[mid])
+                l = mid + 1;
+            else if (!(mid % 2) && nums[mid] == nums[mid + 1])
+                l = mid + 2;
             
-            else l = mid+1;    // else your right half will be odd then update your left pointer to mid+1
+           
+            else h = mid;
         }
         
-        return nums[l];     //left pointer will have the answer at last       
+    
+        return nums[l];    
     }
 };
