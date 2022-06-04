d3135662cc828119031e922c9247fa3bd35869eb
Shubham Kumar <kshubhambgs123@gmail.com>
2022-06-04 09:44:35 +0530

    Time: 15 ms (30.17%), Space: 13.7 MB (64.30%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,53 +1,37 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int temp = 0;
         vector<int> v(2);
-        v[0] = first(nums,target,-1);
-        v[1] = first(nums,target,1);
+        v[0] = search(nums,target,-1); // first occurence
+        v[1] = search(nums,target,1); // last occurence
+        
         return v;
+        
     }
     
-   int  first(vector<int>& nums, int target,int tog) {
-        int low = 0;
-        int high = nums.size()-1;
+    int search(vector<int>& nums,int target,int t){
+        int l = 0;
+        int h =nums.size()-1;
         int idx = -1;
-       while(low<=high){
-           int mid = (high-low)/2 + low;
-           if(nums[mid]<target){
-               low = mid + 1;
-           }
-           else if(nums[mid]>target){
-               high = mid - 1;
-           }
-           else{
-               idx = mid;
-               if(tog==-1) high = mid + tog;
-               else low = mid + tog;
-           }
-       }
-
-           return idx;
-   }
-       
-       
-       // int  last(vector<int>& nums, int target) {
-       //  int low = 0;
-       //  int high = nums.size()-1;
-       //  int idx = -1;
-       // while(low<=high){
-       //     int mid = (high-low)/2 + low;
-       //     if(nums[mid]<=target){
-       //         low = mid + 1;
-       //     }
-       //     else{
-       //         low = mid -1;
-       //     }
-       //     if(nums[mid] == target){
-       //         idx = mid;
-       //     }
-       // }
-       //     return idx;
-       // }
-       
+        
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(nums[mid]<target){
+                l = mid+1;
+            }
+            else if(nums[mid]>target){
+                h = mid -1;
+            }
+            else{ // if(nums[mid]==target)
+                idx = mid; // occurence of target element at idx = mid;
+                if(t==-1)
+                    h = mid + t;
+                else
+                    l = mid + t;
+            }
+        }
+        return idx;
+    }
+    
+    
 };
