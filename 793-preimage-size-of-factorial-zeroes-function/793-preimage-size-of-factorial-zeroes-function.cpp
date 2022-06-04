669ef2a59a5fc2f1ba2b6b88422e57a3ffb4580a
Shubham Kumar <kshubhambgs123@gmail.com>
2022-06-04 13:59:45 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (31.55%) - LeetHub

---
+++ 793-preimage-size-of-factorial-zeroes-function/793-preimage-size-of-factorial-zeroes-function.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int preimageSizeFZF(int k) {
+      
+       int K= k;
+       int last = 1;
+            while (last < K) last = last * 5 + 1;
+            while (last > 1) {
+                K %= last;
+                if (last - 1 == K) return 0;
+                last = (last - 1) / 5;
+            }
+
+            return 5;
+    }
+    
+};
