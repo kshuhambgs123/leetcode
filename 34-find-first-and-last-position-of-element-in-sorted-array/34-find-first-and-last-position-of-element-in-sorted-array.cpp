0cea4099adcba89197d16ad6aa8c7159b0c4ca4f
Shubham Kumar <kshubhambgs123@gmail.com>
2022-06-18 12:22:15 +0530

    Time: 21 ms (8.25%), Space: 13.7 MB (17.91%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,36 +2,35 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> v(2);
-        v[0] = search(nums,target,-1); // first occurence
-        v[1] = search(nums,target,1); // last occurence
+        v[0] = search(nums,target,-1);
+        v[1] = search(nums,target,1);
         
         return v;
-        
     }
-    
-    int search(vector<int>& nums,int target,int t){
+    int search(vector<int>& nums, int target,int tog){
         int l = 0;
-        int h =nums.size()-1;
-        int idx = -1;
+        int h = nums.size()-1;
+        int idx = -1; 
         
         while(l<=h){
             int mid = l + (h-l)/2;
-            if(nums[mid]<target){
+            
+            if(nums[mid]>target){
+                h = mid-1;
+            }
+            else if(nums[mid]<target){
                 l = mid+1;
             }
-            else if(nums[mid]>target){
-                h = mid -1;
-            }
-            else{ // if(nums[mid]==target)
-                idx = mid; // occurence of target element at idx = mid;
-                if(t==-1)
-                    h = mid + t;
+            else{
+                idx = mid;
+                if(tog == -1){
+                    h = mid+tog;
+                }
                 else
-                    l = mid + t;
+                    l = mid+tog;
             }
+            
         }
         return idx;
     }
-    
-    
 };
