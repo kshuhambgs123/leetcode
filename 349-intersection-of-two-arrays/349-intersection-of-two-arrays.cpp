499b8f2a0b268b96e36e5e81c8a8c936d418e35e
Shubham Kumar <kshubhambgs123@gmail.com>
2022-06-20 02:00:55 +0530

    Time: 22 ms (11.27%), Space: 10.2 MB (63.06%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -9,6 +9,7 @@ public:
         
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
+                
                 if(nums1[i]==nums2[j])
                     s.insert(nums1[i]);
             }
