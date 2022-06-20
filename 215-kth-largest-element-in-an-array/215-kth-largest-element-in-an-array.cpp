1b4191178c1d15a6e919a2d93fa7df9e40bceac5
Shubham Kumar <kshubhambgs123@gmail.com>
2022-06-20 16:07:35 +0530

    Time: 1351 ms (5.02%), Space: 9.9 MB (97.43%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -9,7 +9,9 @@ public:
         // bubble sort largest at end
         for(int i=0;i<n-1;i++){
             for(int j = 0;j<n-1-i;j++){
+                
                 if(nums[j]>nums[j+1]){
+                    
                     temp = nums[j];
                     nums[j] = nums[j+1];
                     nums[j+1] = temp;
@@ -18,13 +20,6 @@ public:
             }
             
         }
-        
-        // cout<<nums[n-count]<<" ";
-        // if(count == k){
-        //             ans = nums[n-count];
-        //         }
-        
-        return nums[n-k];
-        
+        return nums[n-k];    
     }
 };
