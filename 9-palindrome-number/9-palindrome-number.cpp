c9c2a2beebefe1d9b5420cd51e50d4136853f6cd
Shubham Kumar <kshubhambgs123@gmail.com>
2022-06-22 22:44:25 +0530

    Time: 16 ms (71.35%), Space: 5.7 MB (90.91%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int t = x/2,p=x;
+        int s=0;
+        if(x<0 || (x>0 && x%10==0)){
+            return false;;
+        }
+        else if(x>=0 && x<=9){
+            return true;
+        }
+        else{
+            while(x>s){
+                int r = x%10;
+                    s = s*10 + r;
+                    x = x/10;
+                
+            }
+            if(x==s || x==s/10){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+};
