93364d46d860aad9d983718682eee062924f8c89
Shubham Kumar <kshubhambgs123@gmail.com>
2022-06-24 20:26:49 +0530

    Time: 1414 ms (14.24%), Space: 237.4 MB (25.49%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-    int count;
-   
-    void checkCount(vector<int>& nums, int start, int mid, int end){
-
-        // two pointers;
-        int l = start, r = mid + 1;
-        while(l <= mid && r <= end){
-            if((long)nums[l] > (long) 2 * nums[r]){
-                count += (mid - l + 1);
-                r++;
-            }else{
-                l++;
-            }
+    int merge(vector<int> &nums, int l, int m, int h) {
+        int count=0;
+        int j=m+1;
+        for(int i=l;i<=m;++i) {
+            while(j<=h && nums[i] > 2LL*nums[j]) ++j;
+            count += (j - (m+1));
         }
-       // worst case might be nlog(n) 
-        sort(nums.begin() + start, nums.begin() + end + 1);
-        return;
-        
-        
-        
-        //every step sort
-    }
-    void mergeSort(vector<int>& nums, int start, int end){
-        if(start == end) return;
-        
-        int mid = (start + end)/2;
-        mergeSort(nums,start, mid);
-        mergeSort(nums,mid+1,end);
-        
-        checkCount(nums,start,mid,end);
-        return;
-        
-    }
-public:
-    int reversePairs(vector<int>& nums) {
-        if(!nums.size())return 0;
-        count = 0;
-        mergeSort(nums,0,nums.size()-1);
+        // merge of two sorted array
+        vector<int> temp;
+        int left=l,right=m+1;
+        // loop till on of them execuested
+        while(left<=m && right<=h) {
+            if(nums[left]<=nums[right]) temp.push_back(nums[left++]);
+            else temp.push_back(nums[right++]);
+        }
+        // remaining elements of left side
+        while(left<=m) temp.push_back(nums[left++]);
+        // remaining elements of right side
+        while(right<=h) temp.push_back(nums[right++]);
+        // assingn in main vector after merging
+        for(int i=l;i<=h;++i) {
+            nums[i]=temp[i-l];
+        }
         return count;
+    }
+    int mergeSort(vector<int> &nums, int l, int h) {
+        if(l>=h) return 0; // A single element does not contain any pair 
+        int m=(l+h)/2;
+        int count=0;
+        count += mergeSort(nums,l,m);    // count from left side of vector
+        count += mergeSort(nums,m+1,h);  // count from right side of vector
+        count += merge(nums,l,m,h);      // count merge 
+        return count;
+    }
+    int reversePairs(vector<int>& nums) {
+        return mergeSort(nums, 0, nums.size()-1);
     }
 };
