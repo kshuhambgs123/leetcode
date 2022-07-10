b1ee0c0f8535fe83822dcce2f03986eaaa2d1251
Shubham Kumar <kshubhambgs123@gmail.com>
2022-07-10 18:37:48 +0530

    Time: 1042 ms (13.52%), Space: 49.8 MB (35.26%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        int n = nums.size();
-        if(n==0){
-            return 0;
+        
+       // TC : O(3N),SC: O(N)
+        unordered_set<int> s;
+        
+        for(int num : nums){  // hash all num into set
+            s.insert(num);
         }
-      
-    unordered_set<int> S;
-    int ans = 0;
- 
-    // Hash all the array elements
-    for (int i = 0; i < n; i++)
-        S.insert(nums[i]);
- 
-    // check each possible sequence from
-    // the start then update optimal length
-    for (int i = 0; i < n; i++)
-    {
-        // if current element is the starting
-        // element of a sequence
-        if (S.find(nums[i] - 1) == S.end())
-        {
-            // Then check for next elements
-            // in the sequence
-            int j = nums[i];
-            while (S.find(j) != S.end())
-                j++;
- 
-            // update  optimal length if
-            // this length is more
-            ans = max(ans, j - nums[i]);
+        
+        int ans = 0;
+        
+        for(int num : nums){
+            if(!s.count(num-1)){  // IF nums-1 is not present int set then num is starting elem/Current elem of subsequence and current length becomes 1
+                int currNum = num;
+                int currLen = 1;
+            //if num+1 is present until consecutive seq is ended set curr num as mnum +1 and currlen as currlen+1
+            while(s.count(currNum+1)){
+                currNum+=1;
+                currLen+=1;
+            }
+            ans = max(ans,currLen); //store  max of length of sequence for each iteration
+        } 
         }
-    }
     return ans;
 }
 };
