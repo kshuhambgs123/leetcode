d4708d76dc8749e81a9a561604ac139af4ad88cf
Shubham Kumar <kshubhambgs123@gmail.com>
2022-07-11 09:59:21 +0530

    Time: 145 ms (47.40%), Space: 41.5 MB (71.10%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -9,19 +9,19 @@ public:
         
         int n = nums.size();
         
-        unordered_map<int,int>mp;
+        unordered_map<int,int>mp; // create a map and take prefix sum and its cnt 
         int currSum = 0;
         int cnt = 0;
         
         for(int i=0;i<n;i++){
-            currSum += nums[i];
+            currSum += nums[i]; // prefix sum till index
             
-            if(currSum == k) cnt++;
+            if(currSum == k) cnt++;  // if prefsum equal to k increase count
             
-            if(mp.count(currSum-k)) 
+            if(mp.count(currSum-k))   // check if prefsum-k is present if present increase cnt 
                 cnt+=mp[currSum-k];
             
-            mp[currSum]++;
+            mp[currSum]++; // update occurence at prefsum
         }
         return cnt;
     }
