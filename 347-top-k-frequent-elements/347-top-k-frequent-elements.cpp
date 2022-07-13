9c4710987f99978645a128c18e2f446449fa44cd
Shubham Kumar <kshubhambgs123@gmail.com>
2022-07-13 13:01:10 +0530

    Time: 35 ms (18.09%), Space: 13.9 MB (17.37%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -2,28 +2,27 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
-         int n = nums.size();
+        int n = nums.size();
         int cnt = 0;
-        map<int,int> freq;
+        map<int,int> freq; // make a freq map store freq of each key
         
         for(int i=0;i<n;i++){
-           
             freq[nums[i]]++;   
         }
         
         vector<pair<int,int>> v;
         vector<int> vec;
         
-        map<int,int>:: iterator it;
-        pair<int,int> p;
-        for(it = freq.begin();it!=freq.end();it++){
-            
+        map<int,int>:: iterator it; // iterator for map
+        pair<int,int> p; // pair for vector
+        for(it = freq.begin();it!=freq.end();it++){ 
             p.first = it->second;
             p.second = it->first;
-            v.push_back(p);
+            v.push_back(p); // store freq and key in vector pair
         }
         
-        sort(v.begin(),v.end(),greater<pair<int,int>>());
+        sort(v.rbegin(),v.rend());
+             //greater<pair<int,int>>()); // sort in decreasing order 
         int n1 = v.size();
         for(int i =0;i<k;i++){
             vec.push_back(v[i].second);
