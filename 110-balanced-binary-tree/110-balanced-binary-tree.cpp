831bd2d9cabc816c413d7e50af5d2e9d2bd7a085
Shubham Kumar <kshubhambgs123@gmail.com>
2022-08-15 18:00:41 +0530

    Time: 32 ms (15.91%), Space: 21.2 MB (15.11%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -11,27 +11,35 @@
  */
 class Solution {
 public:
-    bool res; // SC = O(Ht Of tree) tc : O(n)
     
-    int ht(TreeNode* root){
-       int ans1 = 0,ans2 = 0;
+    int depth(TreeNode* root){
         if(root==NULL)
             return 0;
-        if(!res) // if Answer is already False then return it.
-            return 0;
-        ans1 = ht(root->left);
-        ans2 = ht(root->right);
-        
-        if(abs(ans1-ans2) > 1){
-            res = false;
-        }
-        return 1 + max(ans1, ans2);
+        return 1+max(depth(root->left),depth(root->right));
     }
-    
     bool isBalanced(TreeNode* root) {
-        res = true;
-        int tmp = ht(root);
-        return res;
+        
+        if(root==NULL)
+            return true;
+    queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            TreeNode* curr = q.front();
+            q.pop();
+            
+            int l = depth(curr->left);
+            int r = depth(curr->right);
+            
+            if(abs(l-r)>1)
+                return false;
+            
+            if(curr->left!=NULL)
+                q.push(curr->left);
+            
+            if(curr->right!=NULL)
+                q.push(curr->right);   
+        }
+        return true; 
     }
-      
 };
