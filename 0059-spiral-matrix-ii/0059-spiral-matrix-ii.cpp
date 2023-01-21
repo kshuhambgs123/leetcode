372d33e10b2cb475ca1a02aca31d77b821fdb477
Shubham Kumar <kshubhambgs123@gmail.com>
2023-01-21 08:50:45 +0530

    Time: 4 ms (29.60%), Space: 6.6 MB (23.24%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -6,19 +6,22 @@ public:
         vector<vector<int>> v(n,vector<int>(n));
         
         while(r1<=r2 && c1<=c2){
+            // left to right (row will be fixed)
             for(int i = c1;i<=c2;i++){
                 v[r1][i] = val++;
             }
-            
+            // move down(col will be fixed)
             for(int i = r1+1;i<=r2;i++){
                 v[i][c2] = val++;
             }
-            
+            // move right to left
+            // move  up
             if(r1 < r2 && c1 <c2){
+                // move right to left (row will be fixed)
                 for(int i = c2-1;i>c1;i--){
                     v[r2][i] = val++;
                 }
-                
+                // move up (col will be fixed)
                 for(int i = r2;i>r1;i--){
                     v[i][c1] = val++;
                 }
