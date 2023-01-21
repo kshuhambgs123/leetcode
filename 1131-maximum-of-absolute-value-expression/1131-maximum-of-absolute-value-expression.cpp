7ebf5df9a290c83db1f5b75362e4b0622bf39feb
Shubham Kumar <kshubhambgs123@gmail.com>
2023-01-21 07:56:44 +0530

    Time: 54 ms (59.01%), Space: 24.5 MB (81.37%) - LeetHub

--- 1131-maximum-of-absolute-value-expression/1131-maximum-of-absolute-value-expression.cpp
+++ 1131-maximum-of-absolute-value-expression/1131-maximum-of-absolute-value-expression.cpp
@@ -1,54 +1,38 @@
 class Solution {
 public:
     int maxAbsValExpr(vector<int>& arr1, vector<int>& arr2) {
-       int n = arr1.size();
-        int max1 =INT_MIN ;
+        int n = arr1.size();
+        int max1 = INT_MIN;
+        int max2 = INT_MIN;
+        int max3 = INT_MIN;
+        int max4 = INT_MIN;
+        
         int min1 = INT_MAX;
-        
-        int max2 =INT_MIN ;
         int min2 = INT_MAX;
-        
-        int max3 =INT_MIN ;
         int min3 = INT_MAX;
-        
-        int max4 =INT_MIN ;
         int min4 = INT_MAX;
-    
-        int t1 = 0,t2 =0,t3=0,t4=0;
+            
+        int r1 = 0, r2 = 0, r3 = 0, r4 = 0;
+        
         for(int i=0;i<n;i++){
             
-         
-            max1 = max((arr1[i] + arr2[i] + i),max1);
-          
+            max1 = max(max1,arr1[i]+arr2[i]+i);
+            min1 = min(min1,arr1[i]+arr2[i]+i);
             
-            min1 = min((arr1[i] + arr2[i] + i),min1);
-           
+            max2 = max(max2,arr1[i]+arr2[i]-i);
+            min2 = min(min2,arr1[i]+arr2[i]-i);
             
-            max2  = max((arr1[i] + arr2[i] - i),max2);
-           
+            max3 = max(max3,arr1[i]-arr2[i]+i);
+            min3 = min(min3,arr1[i]-arr2[i]+i);
             
-            min2 = min((arr1[i] + arr2[i] - i),min2);
-           
+            max4 = max(max4,arr1[i]-arr2[i]-i);
+            min4 = min(min4,arr1[i]-arr2[i]-i);
             
-            max3 = max((arr1[i] - arr2[i]-i),max3);
-           
-            
-            min3 = min((arr1[i] - arr2[i] - i),min3);
-           
-        
-            max4 =max((arr1[i] - arr2[i] + i),max4);
-            
-            min4 = min((arr1[i] - arr2[i] + i),min4);
-           
-            t1 = max1-min1;
-            t2 = max2-min2;
-            t3 = max3-min3;
-            t4 = max4-min4;
-            
-            
+            r1 = max1-min1;
+            r2 = max2-min2;
+            r3 = max3-min3;
+            r4 = max4-min4;
         }
-        
-        return max(max(t1,t2),max(t3,t4));
-        
+        return max(max(r2,r1),max(r3,r4));
     }
 };
